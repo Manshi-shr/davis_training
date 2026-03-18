a842b1ad76f2891242a81b21b916ae116dcada7d
Manshi-shr <mantrashr777@gmail.com>
2026-03-18 09:40:02 +0530

    Add files via upload

---
+++ Coding/question02.py
@@ -0,0 +1,10 @@
+#to check if a person is eligible to vote
+
+# Taking input
+age = int(input("Enter your age: "))
+
+# Checking eligibility
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not Eligible to vote")
