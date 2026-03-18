f3cc17ece0773acff926b6a58cdd4369ada718a3
Manshi-shr <mantrashr777@gmail.com>
2026-03-18 09:39:27 +0530

    Implement discount calculation in question01.py
    Added functionality to calculate final price after discount.

---
+++ Coding/question01.py
@@ -0,0 +1,14 @@
+#to calculate final price after discount
+
+# Taking input from user
+price = float(input("Enter the original price: "))
+discount = float(input("Enter discount percentage: "))
+
+# Calculating discount amount
+discount_amount = (price * discount) / 100
+
+# Calculating final price
+final_price = price - discount_amount
+
+# Displaying result
+print("Final price after discount is:", final_price)
