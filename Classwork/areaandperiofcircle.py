2ffeeaf188f8f047b8aa73619478b2d79bd798bd
manshi-shr <mantrashr777@gmail.com>
2026-04-17 10:05:40 +0530

    area and perimeter of circle

---
+++ Classwork/areaandperiofcircle.py
@@ -0,0 +1,13 @@
+# question: area and perimeter of circle and taking input from user
+
+# taking input from user
+radius=float(input("Enter the Radius:"))
+
+# calculating area
+area=3.14*radius*radius
+# calculating perimeter
+perimeter=2*3.14*radius
+
+# printing area and perimeter
+print("Area of Circle:",area)
+print("Perimeter of Circle:",perimeter)
