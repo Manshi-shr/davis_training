0b230014b7316ad4f67f250f6f92301d26a0b130
manshi-shr <mantrashr777@gmail.com>
2026-04-23 10:38:04 +0530

    Addition and substraction of matrix

---
+++ Classwork/numpy/additionofmatrix.py
@@ -0,0 +1,19 @@
+import numpy as np
+#first array
+array1= np.array([10,30,40,50])
+#second array
+array2=np.array([90,70,40,50])
+#---------------------------------
+print("First array:")
+print(array1)
+#---------------------------------
+print("Second array:")
+print(array2)
+#addition of array1 and arrya2
+array3=np.add(array1,array2)
+print("Sum of both array:")
+print(array3)
+#substraction of array1 and array2
+array4=np.subtract(array1,array2)
+print("Substraction of both array:")
+print(array4)
